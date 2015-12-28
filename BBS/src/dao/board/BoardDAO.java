49ced425da430e86e068797cf6e9cedce0c68895
GibeomOh <garmr21@gmail.com>
2015-12-28 22:21:15 +0900

    jsp 작성 중..

--- BBS/src/dao/board/BoardDAO.java
+++ BBS/src/dao/board/BoardDAO.java
@@ -4,9 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.ServletException;
 
 import model.board.BoardModel;
 
@@ -24,11 +23,37 @@ public class BoardDAO {
 	
 	// 목록 조회
     public List<BoardModel> selectList(BoardModel boardModel) {
-        return null;
-    }
-    // 게시판 수 조회
-    public int selectCount(BoardModel boardModel) {
-        return 0;
+    	List<BoardModel> boardList = null;
+    	
+    	try {
+			Class.forName(this.JDBC_DRIVER);
+			this.conn = DriverManager.getConnection(this.DB_URL, this.DB_ID, this.DB_PWD);
+			this.pstmt = this.conn.prepareStatement("SELECT num, subject, email, reg_date, hit FROM board"+ " ORDER BY num DESC ");
+			this.rs = this.pstmt.executeQuery();
+			
+			// 게시판 글 목록 저장
+			boardList = new ArrayList<BoardModel>();
+			while(this.rs.next()) {
+				boardModel = new BoardModel();
+				boardModel.setNum(this.rs.getInt("num"));
+				boardModel.setSubject(this.rs.getString("subject"));
+				boardModel.setEmail(this.rs.getString("email"));
+				boardModel.setRegDate(this.rs.getString("reg_date"));
+				boardModel.setHit(this.rs.getInt("hit"));
+				boardList.add(boardModel);	
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			try { if(rs != null) rs.close(); } catch(Exception e) {}
+			try { if(pstmt != null) pstmt.close(); } catch(Exception e) {}
+			try { if(conn != null) conn.close(); } catch(Exception e) {}
+			
+		}
+    	
+        return boardList;
     }
     
     // 글 선택
@@ -37,7 +62,7 @@ public class BoardDAO {
     		// 데이터베이스 드라이버 로딩
     		Class.forName(this.JDBC_DRIVER);
     		// 커넥션 정보 저장
-    		this.conn = DriverManager.getConnection(DB_URL, DB_URL, DB_PWD);
+    		this.conn = DriverManager.getConnection(DB_URL, DB_ID, DB_PWD);
     		// Query 설정
     		this.pstmt = this.conn.prepareStatement("SELECT num, subject, contents, email, hit, reg_date FROM board"+ " WHERE num = ?");
     		this.pstmt.setInt(1, boardModel.getNum());
@@ -71,16 +96,36 @@ public class BoardDAO {
     
     // 글 등록
     public void insert(BoardModel boardModel) {
-    	
+    	try {
+    		Class.forName(this.JDBC_DRIVER);
+    		this.conn = DriverManager.getConnection(DB_URL, DB_ID, DB_PWD);
+    		this.pstmt = this.conn.prepareStatement("INSERT INTO board (subject, emil, contents, hit, reg_date, mod_date)"+ " VALUES (?, ?, ?, 0, NOW(), MEW())");
+    		this.pstmt.setString(1, boardModel.getSubject());
+			this.pstmt.setString(2, boardModel.getEmail());
+			this.pstmt.setString(3, boardModel.getContents());
+    		this.pstmt.executeUpdate();
+    		
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    		
+    	} finally {
+			try { if(rs != null) rs.close(); } catch(Exception e) {}
+			try { if(pstmt != null) pstmt.close(); } catch(Exception e) {}
+			try { if(conn != null) conn.close(); } catch(Exception e) {}
+			
+    	}
     }
     
     // 글 수정
     public void update(BoardModel boardModel) {
     	try {
     		Class.forName(this.JDBC_DRIVER);
-    		this.conn = DriverManager.getConnection(DB_URL, DB_URL, DB_PWD);
-    		this.pstmt = this.conn.prepareStatement("UPDATE board SET hit = hit + 1"+ " WHERE num = ?");
-    		this.pstmt.setInt(1, boardModel.getNum());
+    		this.conn = DriverManager.getConnection(DB_URL, DB_ID, DB_PWD);
+    		this.pstmt = this.conn.prepareStatement("UPDATE board SET subject = ?, emil = ?, contents = ?, mod_date = NOW()"+ " WHERE num = ?");
+    		this.pstmt.setString(1, boardModel.getSubject());
+			this.pstmt.setString(2, boardModel.getEmail());
+			this.pstmt.setString(3, boardModel.getContents());
+			this.pstmt.setInt(4, boardModel.getNum());
     		this.pstmt.executeUpdate();
     		
     	} catch(Exception e) {
@@ -98,7 +143,7 @@ public class BoardDAO {
     public void updateHit(BoardModel boardModel) {
     	try {
     		Class.forName(this.JDBC_DRIVER);
-    		this.conn = DriverManager.getConnection(DB_URL, DB_URL, DB_PWD);
+    		this.conn = DriverManager.getConnection(DB_URL, DB_ID, DB_PWD);
     		this.pstmt = this.conn.prepareStatement("UPDATE board SET hit = hit + 1"+ " WHERE num = ?");
     		this.pstmt.setInt(1, boardModel.getNum());
     		this.pstmt.executeUpdate();
@@ -118,7 +163,7 @@ public class BoardDAO {
     public void delete(BoardModel boardModel) {
     	try {
     		Class.forName(this.JDBC_DRIVER);
-    		this.conn = DriverManager.getConnection(DB_URL, DB_URL, DB_PWD);
+    		this.conn = DriverManager.getConnection(DB_URL, DB_ID, DB_PWD);
     		this.pstmt = this.conn.prepareStatement("DELETE FROM board"+ " WHERE num = ?");
     		this.pstmt.setInt(1, boardModel.getNum());
     		this.pstmt.executeUpdate();
